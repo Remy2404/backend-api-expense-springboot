6c32a91ba96ff05f3d06ef72e5ed0ba6708b044c
remy2404 <meemii191@gmail.com>
2026-02-24 11:52:59 +0700

    feat(dto): add forecast risk category object

---
+++ src/main/java/com/wing/backendapiexpensespringboot/dto/ForecastRiskCategory.java
@@ -0,0 +1,16 @@
+package com.wing.backendapiexpensespringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ForecastRiskCategory {
+
+    private String category;
+    private String reason;
+}
