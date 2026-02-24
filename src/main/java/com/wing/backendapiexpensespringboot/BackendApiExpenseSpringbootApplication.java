e7a3f0f3b8c6e0a757a63e729da974240c2921d8
remy2404 <meemii191@gmail.com>
2026-02-24 11:52:56 +0700

    feat(core): add main application class

---
+++ src/main/java/com/wing/backendapiexpensespringboot/BackendApiExpenseSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wing.backendapiexpensespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiExpenseSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApiExpenseSpringbootApplication.class, args);
+    }
+
+}
