eb08aaa911f82c4514d4f4ecfd0c0d23d3acf94a
remy2404 <meemii191@gmail.com>
2026-02-24 11:53:00 +0700

    feat(model): add category entity

---
+++ src/main/java/com/wing/backendapiexpensespringboot/model/CategoryEntity.java
@@ -0,0 +1,37 @@
+package com.wing.backendapiexpensespringboot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "firebase_uid", nullable = false)
+    private String firebaseUid;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "icon")
+    private String icon;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "created_at")
+    private String createdAt;
+}
