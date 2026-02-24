f1ea7232cf75d644e8522037a99fabe1d14adce9
remy2404 <meemii191@gmail.com>
2026-02-24 11:52:59 +0700

    feat(dto): add insight period object

---
+++ src/main/java/com/wing/backendapiexpensespringboot/dto/InsightPeriod.java
@@ -0,0 +1,18 @@
+package com.wing.backendapiexpensespringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InsightPeriod {
+
+    private LocalDate start;
+    private LocalDate end;
+}
