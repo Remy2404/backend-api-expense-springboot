449b481a64e4bcdb547b15ac004278814e453831
remy2404 <meemii191@gmail.com>
2026-02-24 11:53:03 +0700

    test(core): add application context load tests

---
+++ src/test/java/com/wing/backendapiexpensespringboot/BackendApiExpenseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wing.backendapiexpensespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiExpenseSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
